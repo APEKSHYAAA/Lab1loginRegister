d9d635d0b0f7b9b17daacf3f3032d09f2244dfe7
appu <apu58728@gmail.com>
2025-02-27 02:46:43 +0545

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()]
 })
